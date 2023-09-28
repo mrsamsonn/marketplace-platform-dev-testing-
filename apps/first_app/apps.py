3986fe293def5142ead88ae82b3efda019369c8d
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-28 15:33:07 -0700

    stable

--- apps/first_app/apps.py
+++ apps/first_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FirstAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'first_app'
+# class FirstAppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'first_app'
